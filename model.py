8f94dabdf397feb56b3837d8e49686e8919eae96
DEVAN\DEVANZ <devanlustig@gmail.com>
2024-11-08 15:47:53 +0700

    new function execute for global action

--- model.py
+++ model.py
@@ -11,3 +11,17 @@ class Karyawan:
     def insert_data(self, query, values=None):
         
         return insert(query, values, self.mydb)
+
+    def execute_query(self, query, values=None):
+        try:
+            cursor = self.mydb.cursor(dictionary=True)
+            cursor.execute(query, values)
+            if query.strip().upper().startswith('SELECT'):
+                return cursor.fetchall()  # Mengambil hasil query SELECT
+            else:
+                self.mydb.commit()  # Jika bukan SELECT, melakukan commit untuk operasi DML (INSERT, UPDATE, DELETE)
+        except Exception as e:
+            self.mydb.rollback()  # Menggagalkan transaksi jika terjadi error
+            raise e
+        finally:
+            cursor.close()
